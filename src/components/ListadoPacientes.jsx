3b5dab4e8679bb583631a6a19de2dce2f5a679ea
devCraf <caimgelus@gmail.com>
2022-10-18 16:21:28 -0500

    segundo commit

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -4,6 +4,35 @@ const ListadoPacientes = ({ pacientes }) => {
   return (
     <>
       <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+        {pacientes && pacientes.lenght ? (
+          <>
+            <h2 className="font-black text-3xl text-center">
+              Lisatado Pacientes
+            </h2>
+            <p className="text-xl mt-5 mb-5 text-center">
+              Administra tu {""}
+              <span className="text-indigo-600 font-bold ">
+                Pacientes y citas
+              </span>
+            </p>
+            {pacientes.map((paciente) => (
+              <Paciente key={paciente.id} paciente={paciente} />
+            ))}
+          </>
+        ) : (
+          <>
+            <h2 className="font-black text-3xl text-center">
+              Lisatado Pacientes
+            </h2>
+            <p className="text-xl mt-5 mb-5 text-center">
+              Administra tu {""}
+              <span className="text-indigo-600 font-bold ">
+                Pacientes y citas
+              </span>
+            </p>
+          </>
+        )}
+
         <h2 className="font-black text-3xl text-center">Lisatado Pacientes</h2>
         <p className="text-xl mt-5 mb-5 text-center">
           Administra tu {""}
